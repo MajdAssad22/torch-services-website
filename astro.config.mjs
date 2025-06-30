6e8ec67cf6ffc29071d4d6aff72949cd74c23913
Majd Assad <majdassad123@gmail.com>
2025-06-30 09:28:05 -0400

    Fixing astro on netlify routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import netlify from '@astrojs/netlify';
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
   vite: {
-      plugins: [tailwindcss()]
-    },
+    plugins: [tailwindcss()],
+  },
 
   integrations: [react()],
-  adapter: netlify()
+  adapter: netlify(),
 });
